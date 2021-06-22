b40b11e95e4600ee19e9efc55c9ba7853ab32892
Sivasankar <ss.madala22@gmail.com>
2021-06-22 22:10:12 +0530

    experience component added

---
+++ src/pages/experience/experience.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Tilt from "react-tilt";
+import Card from "react-bootstrap/Card";
+import L_ACCENTURE from "../../assets/img/experience/accenture-3.svg";
+
+const Experience = () => {
+    return (
+        <div id='experience'>
+            
+        </div>
+    )
+}
+
+export default Experience
